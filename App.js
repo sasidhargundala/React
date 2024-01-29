05bd914bd89c58c940d926486e60b3826b310dae
sasidhargundala <117711272+sasidhargundala@users.noreply.github.com>
2024-01-29 07:53:17 +0530

    worked on 25,27

---
+++ App.js
@@ -0,0 +1,21 @@
+import React,{ Component } from 'react';
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+class App extends Component {
+  render(){
+    return(
+    <div className="App">
+      
+      <Greet/>
+      <Welcome/>
+      <Hello/>
+      
+    </div>
+  );
+}
+}
+
+export default App;
+
